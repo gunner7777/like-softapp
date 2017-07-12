b62394d4bf511964d4b9f38f2962efc6ccd2890c
gunner7777 <123@yandex.ru>
2017-07-12 08:58:00 +0300

    first commit w/o code

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var pug = require('gulp-pug');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+var input = './src/sass/**/*.scss';
+var output = './build/css';
+var pugInput = './src/views/*.pug';
+var jsInput = './src/js/*.js';
+var jsOutput = './build/js';
+var fontsInput = './src/fonts/*.*';
+var fontsOutput = './build/fonts';
+var vendorInput = './src/vendor/*.*';
+var vendorOutput = './build/vendor';
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: './build'
+    },
+    notify: false
+  });
+});
+
+gulp.task('html', function buildHTML() {
+  return gulp.src(pugInput)
+  .pipe(pug({
+    pretty: true 
+  }))
+  .pipe(rename('index.html'))
+  .pipe(gulp.dest('./build'))
+  .pipe(browserSync.reload({
+    stream: true
+  }));
+});
+
+gulp.task('sass', function() {
+  return gulp.src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename('style.css'))
+    //.pipe(concat('style.css'))
+    .pipe(gulp.dest(output))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('js', function() {
+  return gulp.src(jsInput)
+    .pipe(concat('hamintec.js'))
+    .pipe(gulp.dest(jsOutput))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(fontsInput)
+    .pipe(gulp.dest(fontsOutput))
+});
+
+gulp.task('vendor', function() {
+  return gulp.src(vendorInput)
+    .pipe(gulp.dest(vendorOutput))
+});
+
+
+gulp.task('watch', ['browserSync', 'sass', 'js', 'html', 'fonts', 'vendor'], function() {
+  gulp.watch(input, ['sass']);
+  gulp.watch(pugInput, ['html']);
+  gulp.watch(jsInput, ['js']);
+});
