a04eb9ae64d70d5271abd94fa7ca6b8e2e1d5c16
gunner7777 <123@yandex.ru>
2017-07-28 16:45:32 +0300

    brainfuck

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
   var parent = document.querySelectorAll('.parent');
   parent.forEach(function(item) {
-    item.addEventListener('mouseenter', toggleMenu);
-    item.addEventListener('mouseleave', toggleMenu);
+    item.addEventListener('mouseenter', showMenu);
+    item.addEventListener('mouseleave', function(e) {
+      hideMenu(e);
+    });
+  });
+
+
+  var submenu = document.querySelectorAll('.submenu');
+  submenu.forEach(function(item) {
+    item.addEventListener('mouseenter', showMenu);
+    item.addEventListener('mouseleave', function(e) {
+      hideMenu(e);
+    });
   });
 });
 
-function toggleMenu() {
-  console.log(this.nextSibling.nextSibling);
-  this.nextSibling.nextSibling.classList.add('submenu--open');
+function showMenu() {
+  if(!this.classList.contains('main-menu__link--active')) {
+    this.classList.add('main-menu__link--active');
+    this.nextElementSibling.classList.add('submenu--show');
+  }
+
+
+  if(this.nextElementSibling.classList.contains('submenu--show')) {
+    console.log(this);
+    this.nextElementSibling.addEventListener('mouseenter', function() {
+      console.log("target");
+      this.removeEventListener('mouseleave', hideMenu);
+    });
+  	//this.nextElementSibling.classList.remove('submenu--show')
+  }
+}
+
+function hideMenu(e) {
+  setTimeout(function() {
+    console.log(e.target);
+    //this.classList.remove('main-menu__link--active');
+    //this.nextElementSibling.classList.remove('submenu--show');
+  }, 1000);
 }
