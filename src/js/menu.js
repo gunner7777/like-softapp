2bab88e511cff4b441696c556a96bab2a438b2b5
gunner7777 <123@yandex.ru>
2017-08-23 09:20:31 +0300

    merge

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,138 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   mainMenu.addEventListener('mouseout', function(e) {
-    if(e.target.classList.contains('.active') && (!e.target.contains(e.relatedTarget))) {
-      e.target.querySelector('.submenu').classList.remove('submenu--show');
-    }
-  });
-});
-
-
-
-
-
-
-
-
-
-/*document.addEventListener('DOMContentLoaded', function() {
-  var mainMenu = document.querySelector('.main-menu');
-
-  mainMenu.addEventListener('mouseover', function(e) {
-    if(e.target.classList.contains('parent')) {
-      e.target.nextElementSibling.classList.add('submenu--show');
-    }
-  });
-
-  /*mainMenu.addEventListener('mouseout', function(e) {
-    setTimeout(function() {
-    if(!e.relatedTarget.parentNode.classList.contains('submenu') && !e.relatedTarget.parentNode.classList.contains('submenu__item')) {
-      console.log(e.relatedTarget.tagName);
-      
-        if(e.target.classList.contains('parent')) {
-          //e.target.nextElementSibling.classList.remove('submenu--show');
-        }
-      
-    }
-    }, 800);
-  });*/
-
-
-  /*var subMenu = document.querySelector('.submenu');
-
-  subMenu.addEventListener('mouseover', function(e) {
-    
-  });
-
-  subMenu.addEventListener('mouseout', function(e) {
-    
-  });
-
-  window.addEventListener('resize', function() {
-    console.log('resize');
-  });
-});*/
-
-
-
-/*document.addEventListener('DOMContentLoaded', function() {
-  var mainMenu = document.querySelector('.main-menu');
-
-  mainMenu.addEventListener('mouseover', function(e) {
-    if(e.target.classList.contains('parent')) {
-      e.target.nextElementSibling.classList.add('submenu--show');
-    }
-  });
-
-  mainMenu.addEventListener('mouseout', function(e) {
-    setTimeout(function() {
-      if(!e.relatedTarget.classList.contains('parent')) {
-        if(!e.relatedTarget.classList.contains('submenu--show')) {
-          e.target.nextElementSibling.classList.remove('submenu--show');
-        }
+      if(e.target.classList.contains('active') && (!e.target.contains(e.relatedTarget))) {
+        e.target.querySelector('.submenu').classList.remove('submenu--show');
+        e.target.classList.remove('active');
       }
-      /*if(!e.relatedTarget.classList.contains('submenu--show')) {
+      
+      if(!mainMenu.contains(e.relatedTarget) || (e.relatedTarget.classList.contains('main-menu__item'))) {
+        var subm = mainMenu.querySelectorAll('.submenu');
+        subm.forEach(function(item) {
+          item.classList.remove('submenu--show');
+        });
 
-        //удалить того чего нет невозможно
-        e.relatedTarget.classList.remove('submenu--show');
-      }*/
-    /*}, 800);
-  });
-});*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*document.addEventListener('DOMContentLoaded', function() {
-  var parent = document.querySelectorAll('.parent');
-  parent.forEach(function(item) {
-    item.addEventListener('mouseenter', showMenu);
-    item.addEventListener('mouseleave', function(e) {
-      hideMenu(e);
-    });
-  });
-
-
-  var submenu = document.querySelectorAll('.submenu');
-  submenu.forEach(function(item) {
-    item.addEventListener('mouseenter', showMenu);
-    item.addEventListener('mouseleave', function(e) {
-      hideMenu(e);
-    });
+        var active = mainMenu.querySelectorAll('.active');
+        active.forEach(function(item) {
+          item.classList.remove('active');
+        });
+      }
   });
 });
-
-function showMenu() {
-  if(!this.classList.contains('main-menu__link--active')) {
-    this.classList.add('main-menu__link--active');
-    this.nextElementSibling.classList.add('submenu--show');
-  }
-
-
-  if(this.nextElementSibling.classList.contains('submenu--show')) {
-    console.log(this);
-    this.nextElementSibling.addEventListener('mouseenter', function() {
-      console.log("target");
-      this.removeEventListener('mouseleave', hideMenu);
-    });
-  	//this.nextElementSibling.classList.remove('submenu--show')
-  }
-}
-
-function hideMenu(e) {
-  setTimeout(function() {
-    console.log(e.target);
-    //this.classList.remove('main-menu__link--active');
-    //this.nextElementSibling.classList.remove('submenu--show');
-  }, 1000);
-}*/
